930651e9733c78d229feff9c193b7b388cdadf9d
Thomas Fourier <fourier.thomas@gmail.com>
2024-11-18 11:51:40 +0100

    add javadoc to updateRelasateurCelebre

--- src/main/java/com/ensta/myfilmlist/service/MyFilmsService.java
+++ src/main/java/com/ensta/myfilmlist/service/MyFilmsService.java
@@ -4,5 +4,11 @@ import com.ensta.myfilmlist.model.Realisateur;
 
 
 public interface MyFilmsService {
+    /**
+     * Met à jour le statut célèbre d'un réalisateur.
+     * @param Real Rélalisateur à mettre à jour.
+     * @return Retourne le réalisateur mis à jour.
+     * @throws ServiceException En cas d'érreur.
+     */
     public Realisateur updateRealisateurCelebre(Realisateur Real) throws ServiceException ;
 }
