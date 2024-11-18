ad19ef202d4cab034919d0520fff3af85ec8650d
Thomas Fourier <fourier.thomas@gmail.com>
2024-11-18 10:38:29 +0100

    change template

---
+++ src/main/java/com/ensta/myfilmlist/model/Film.java
@@ -0,0 +1,38 @@
+package com.ensta.myfilmlist.model;
+
+/**
+ * Represente un Film.
+ */
+public class Film {
+
+	private long id;
+
+	private String titre;
+
+	private int duree;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public int getDuree() {
+		return duree;
+	}
+
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+
+}
