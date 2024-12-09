a6113814135ee2ccaa100a4a041a7de4a3aaf870
wakaroquo <gabriel@hazera.net>
2024-12-09 11:24:34 +0100

    Partie 5 7.1

--- my-film/src/FilmCard.js
+++ my-film/src/FilmCard.js
@@ -1,17 +1,34 @@
 import Typography from '@mui/material/Typography';
-import { Card } from '@mui/material';
+import { Card, IconButton } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function FilmCard(props) {
-    return <Card variant="outlined">
-        <CardContent>
-            <Typography variant="h5" gutterBottom>
-                {props.film.titre}
-            </Typography>
-            <Typography variant="body1">
-                {props.film.duree} minutes
-            </Typography>
-        </CardContent>
-    </Card>
-    ;
+    const handleClickOnDeleteButton = () => {
+        console.log("delete");
+      }
+    
+      const handleClickOnEditButton = () => {
+        console.log("edit");
+      }
+
+    return (
+        <Card variant="outlined">
+            <CardContent>
+                <Typography variant="h5" gutterBottom>
+                    {props.film.titre}
+                </Typography>
+                <Typography variant="body1">
+                    {props.film.duree} minutes
+                </Typography>
+                <IconButton onClick={handleClickOnDeleteButton}>
+                    <DeleteIcon />
+                </IconButton>
+                <IconButton onClick={handleClickOnEditButton}>
+                    <EditIcon />
+                </IconButton>
+            </CardContent>
+        </Card>
+    );
 }
